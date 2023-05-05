edade2e81eca2ef1e41bfe3f24c1bee2d90f4e41
Ailen Goitia <ailugoitia96@gmail.com>
2023-05-05 00:09:08 -0300

    primer commit

---
+++ src/componentes/navBar/NavBar.js
@@ -0,0 +1,17 @@
+import CardWidgets from "./cardWidgets/cardWidgets";
+const NavBar = () =>{
+    return(
+        <nav>
+            <h3>Narices Frias</h3>
+            <div>
+                <button className="boton-navbar">Alimentos</button>
+                <button className="boton-navbar">Accesorios</button>
+                <button className="boton-navbar">Higiene</button>
+                <button className="boton-navbar">Juguetes</button>
+            </div>
+            <CardWidgets/>
+        </nav>
+    )
+}
+
+export default NavBar;
---
+++ src/componentes/navBar/cardWidgets/cardWidgets.js
@@ -0,0 +1,10 @@
+const CardWidgets = () => {
+    return (
+        <div>
+            <h3>🛒 0</h3>
+        </div>
+
+    )
+
+}
+export default CardWidgets;
